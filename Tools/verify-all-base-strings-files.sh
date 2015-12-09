600e42abeaf2a86ca5690625fa98d7871009cbb0
Daniel Kennett <daniel@cascable.se>
2015-12-09 17:14:10 +0100

    Add strings file tools

---
+++ Tools/verify-all-base-strings-files.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+./verify-string-files -warning-level warning -master ../Base.lproj/GeneralUI.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/InfoPlist.strings
+
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonAEModes.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonApertureValues.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonBatteryPowers.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonBatterySelects.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonBracketModes.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonDriveModes.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonExposureCompensationValues.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonFocusModes.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonISOValues.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonLensStatuses.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonLVAFSystems.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonMirrorLockupEnabledStates.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonMirrorLockupStages.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonShortPropertyCodes.strings
+./verify-string-files -warning-level warning -master ../Base.lproj/CanonShutterSpeeds.strings
